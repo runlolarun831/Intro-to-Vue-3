382f82e9fecae683e78e16eb6b0bb9c76eefa390
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2021-08-21 13:47:57 -0700

    fourth commit

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green'},
+              { id: 2235, color: 'blue'}
+            ],
+            sizes: [
+              {id: 1234, size: 'x-small'}, {id: 1235, size: 'small'},{id: 1236, size: 'medium'},{id: 1237, size: 'large'},{id: 1238, size: 'x-large'}
+            ]
         }
     }
 })
