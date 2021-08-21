ba32a56b4224023be74dbe1cec584d93408e1936
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2021-08-21 12:35:27 -0700

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.redbubble.com/i/socks/Vue-JS-Pattern-by-hipstuff/27051475.9HZ1B'
         }
     }
 })
